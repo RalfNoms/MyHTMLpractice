8eb017f169bc12edbe408227763bcfa5c87448d9
ralfnoms <ralfjonenomos@gmail.com>
2023-03-24 08:14:50 +0800

    initial

---
+++ script.js
@@ -0,0 +1,23 @@
+let students = [
+
+    {id: 1 , name: 'tom'},
+    {id: 2 , name: 'tim'},
+    {id: 3 , name: 'tym'},
+];
+
+let add = ["ralf"]
+
+let info = students.push(add)
+
+console.log(add + "/ " + "was added to the student's list.")
+console.log(students)
+
+
+
+
+
+
+
+
+
+
